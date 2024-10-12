290fb14f4845ada52441383b4f4271224fd020df
Moises <35911832+CamachoMoises@users.noreply.github.com>
2024-10-12 07:58:55 -0400

    Update: edit course form

--- src/pages/pages/nuevoCurso.tsx
+++ src/pages/pages/nuevoCurso.tsx
@@ -1,4 +1,7 @@
 import {
+	Accordion,
+	AccordionBody,
+	AccordionHeader,
 	Card,
 	CardBody,
 	Checkbox,
@@ -9,12 +12,16 @@ import {
 	Typography,
 } from '@material-tailwind/react';
 import { useState } from 'react';
-import { participante } from '../../types/utilidades';
+import { moduloTeoria, participante } from '../../types/utilidades';
 
 const NuevoCurso = () => {
+	const [open, setOpen] = useState(1);
+
+	const handleOpen = (value: number) =>
+		setOpen(open === value ? 0 : value);
 	const [participanteSelec, setParticipanteSelec] =
 		useState<participante | null>();
-	const participantes: participante[] = [
+	const personal: participante[] = [
 		{
 			id: 1,
 			name: 'Juan',
@@ -42,6 +49,52 @@ const NuevoCurso = () => {
 			country: 'Uruguay',
 			tipo: 1,
 		},
+		{
+			id: 4,
+			name: 'Ana',
+			lastName: 'Rodriguez',
+			nro_doc: 22222222,
+			email: 'pedro.lopez@gmail.com',
+			country: 'Venezuela',
+			tipo: 2,
+		},
+		{
+			id: 5,
+			name: 'Jose',
+			lastName: 'Clark',
+			nro_doc: 33333333,
+			email: 'jose.Clark@gmail.com',
+			country: 'Argentina',
+			tipo: 2,
+		},
+	];
+	const participantes: participante[] = personal.filter(
+		(part) => part.tipo === 1
+	);
+	const instructores: participante[] = personal.filter(
+		(part) => part.tipo === 2
+	);
+	const modulos: moduloTeoria[] = [
+		{
+			id: 0,
+			name: 'Bienvenida/Papeleo',
+			hours: 0.25,
+		},
+		{
+			id: 1,
+			name: 'Generalidades de la Aeronave/ Procedimientos Operacionales / Plan de vuelo/ Maniobras. ',
+			hours: 7.75,
+		},
+		{
+			id: 2,
+			name: 'Peso y Balance, Planificación y  Performance ',
+			hours: 8,
+		},
+		{
+			id: 3,
+			name: 'Sistema eléctrico, Iluminación y Panel de Advertencia',
+			hours: 3,
+		},
 	];
 	const handlePartipante = (value: string | undefined) => {
 		const ps = participantes.find(
@@ -63,7 +116,7 @@ const NuevoCurso = () => {
 					onPointerEnterCapture={undefined}
 					onPointerLeaveCapture={undefined}
 				>
-					<div className="c">
+					<div className="">
 						<Typography
 							variant="h5"
 							placeholder={undefined}
@@ -101,14 +154,40 @@ const NuevoCurso = () => {
 
 							{participanteSelec && (
 								<>
-									<Typography
-										variant="small"
-										placeholder={undefined}
-										onPointerEnterCapture={undefined}
-										onPointerLeaveCapture={undefined}
-									>
-										{participanteSelec.nro_doc}
-									</Typography>
+									<div className="flex flex-row gap-3">
+										<Typography
+											variant="h6"
+											placeholder={undefined}
+											onPointerEnterCapture={undefined}
+											onPointerLeaveCapture={undefined}
+										>
+											Número de Identificación <br /> del Piloto
+										</Typography>
+										<Typography
+											variant="paragraph"
+											placeholder={undefined}
+											onPointerEnterCapture={undefined}
+											onPointerLeaveCapture={undefined}
+										>
+											{participanteSelec.nro_doc}
+										</Typography>
+										<Typography
+											variant="h6"
+											placeholder={undefined}
+											onPointerEnterCapture={undefined}
+											onPointerLeaveCapture={undefined}
+										>
+											Pais
+										</Typography>
+										<Typography
+											variant="paragraph"
+											placeholder={undefined}
+											onPointerEnterCapture={undefined}
+											onPointerLeaveCapture={undefined}
+										>
+											{participanteSelec.country}
+										</Typography>
+									</div>
 								</>
 							)}
 							<Input
@@ -229,6 +308,113 @@ const NuevoCurso = () => {
 					</div>
 
 					<hr />
+					<Accordion
+						open={open === 1}
+						placeholder={undefined}
+						onPointerEnterCapture={undefined}
+						onPointerLeaveCapture={undefined}
+					>
+						<AccordionHeader
+							onClick={() => handleOpen(1)}
+							placeholder={undefined}
+							onPointerEnterCapture={undefined}
+							onPointerLeaveCapture={undefined}
+						>
+							Modulos del curso
+						</AccordionHeader>
+						<AccordionBody>
+							<div className="flex flex-col gap-2 ">
+								{modulos.map((modulo) => (
+									<div
+										key={modulo.id}
+										className="flex flex-row gap-4 rounded-md border border-blue-gray-300"
+									>
+										<div className="grid grid-cols-4 gap-4 py-2 px-2">
+											<div className="flex flex-row gap-2">
+												<Typography
+													variant="h6"
+													className="w-60"
+													placeholder={undefined}
+													onPointerEnterCapture={undefined}
+													onPointerLeaveCapture={undefined}
+												>
+													{modulo.name}
+												</Typography>
+											</div>
+											<div className="flex flex-row gap-2">
+												<Input
+													type="date"
+													label="Fecha"
+													required={true}
+													crossOrigin={undefined}
+													onPointerEnterCapture={undefined}
+													onPointerLeaveCapture={undefined}
+												/>
+											</div>
+											<div className="flex flex-row gap-2">
+												<Input
+													type="number"
+													inputMode="numeric"
+													label="Horas "
+													className="appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+													onPointerEnterCapture={undefined}
+													onPointerLeaveCapture={undefined}
+													crossOrigin={undefined}
+												/>
+												<Typography
+													variant="h6"
+													className="w-60"
+													placeholder={undefined}
+													onPointerEnterCapture={undefined}
+													onPointerLeaveCapture={undefined}
+												>
+													Max:{modulo.hours}
+												</Typography>
+											</div>
+											<div className="flex flex-row gap-2">
+												<Select
+													label="Selecionar Instructor"
+													placeholder={undefined}
+													onPointerEnterCapture={undefined}
+													onPointerLeaveCapture={undefined}
+												>
+													{instructores.map((instr) => (
+														<Option
+															key={instr.id}
+															value={`${instr.id}`}
+														>
+															{instr.name} {instr.lastName}
+														</Option>
+													))}
+												</Select>
+											</div>
+										</div>
+									</div>
+								))}
+							</div>
+						</AccordionBody>
+					</Accordion>
+					<Accordion
+						open={open === 2}
+						placeholder={undefined}
+						onPointerEnterCapture={undefined}
+						onPointerLeaveCapture={undefined}
+					>
+						<AccordionHeader
+							onClick={() => handleOpen(2)}
+							placeholder={undefined}
+							onPointerEnterCapture={undefined}
+							onPointerLeaveCapture={undefined}
+						>
+							Resultados
+						</AccordionHeader>
+						<AccordionBody>
+							We&apos;re not always in the position that we want to be
+							at. We&apos;re constantly growing. We&apos;re constantly
+							making mistakes. We&apos;re constantly trying to express
+							ourselves and actualize our dreams.
+						</AccordionBody>
+					</Accordion>
 				</CardBody>
 			</Card>
 		</div>
